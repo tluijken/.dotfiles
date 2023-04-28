772a8b8423d922706716d0453b228a0772743943
Thomas Luijken <tluijken@outlook.com>
2023-04-28 16:59:03 +0200

    Coloring always

--- nvim/.config/nvim/after/plugin/colors.lua
+++ nvim/.config/nvim/after/plugin/colors.lua
@@ -8,6 +8,4 @@ function ColorMyPencils(color)
   vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
 end
 
-if isModuleAvailable("gruvbox-material") then
-  ColorMyPencils()
-end
+ColorMyPencils()
